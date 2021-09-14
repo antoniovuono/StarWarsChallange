797a0177184fc1ef81d1b17f886cefc24ce43fa7
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2021-09-14 18:43:02 -0300

    Stack Navigation
    Bottom navigation changed for stacknavigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
-import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+
+import { createStackNavigator } from '@react-navigation/stack';
 
 import { Home } from '../screens/Home';
+import { Favorites } from '../screens/Favorites';
 import { CharacterDetails } from '../screens/CharacterDetails';
 
-const { Navigator, Screen } = createBottomTabNavigator();
+
+const { Navigator, Screen } = createStackNavigator ();
 
 
 export function AppRoutes() {
+
     return (
-        <Navigator>
-            <Screen
-                name="Home"
-                component={Home}
-            />
-            <Screen 
-                name="CharacterDetails"
-                component={CharacterDetails}
-            />
+        <Navigator
+          headerMode="none"
+        >
+                <Screen
+                    name="Home"
+                    component={Home}
+                
+                />
+                <Screen 
+                    name="Favorites"
+                    component={Favorites}
+                    
+                />
+
+                <Screen 
+
+                    name="CharacterDetails"
+                    component={CharacterDetails}
+
+                />
+
+
+
         </Navigator>
+
     );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,8 @@ import { AppRoutes } from './app.routes';
 export function Routes() {
     return (
         <NavigationContainer>
-            <AppRoutes />
+            <AppRoutes /> 
         </NavigationContainer>
+    
     );
 }
